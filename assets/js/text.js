ad50d4fb9504cd06d657cece11b9981dc2a22d57
Jaffar1999 <jaffarpopneiz@gmail.com>
2024-07-09 17:59:07 +0530

    first commit

---
+++ assets/js/text.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: ["Web Designer", "Software Engineer", "Software support", "Content Creator","Product support"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true,
+        cursorChar: '|',
+        smartBackspace: true
+    };
+
+    var typed = new Typed('#typed', options);
+});
